9955d0f733ffcc9d198dcb60aa9d04993f91ca9c
Guy Koifman <guykoifman45@gmail.com>
2023-02-12 20:56:48 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+import pywhatkit
+Message = 'היי בובה:]]\n זאת הודעה בעברית'
+pywhatkit.sendwhatmsg_instantly(phone_no='+972585845386', message=Message, wait_time=15, tab_close=True, close_time=10)
+
+
+
