2445601aff16a668a7c218500341a0612f61192b
Guy Koifman <guykoifman45@gmail.com>
2023-02-12 21:22:47 +0200

    added the possibility to send an img to a group

--- main.py
+++ main.py
@@ -2,7 +2,21 @@
 
 import pywhatkit
 Message = 'היי בובה:]]\n זאת הודעה בעברית'
-pywhatkit.sendwhatmsg_instantly(phone_no='+972585845386', message=Message, wait_time=15, tab_close=True, close_time=10)
+wait_time_for_writing_msg_in_chrome = 10
+wait_time_to_close_tab = 2
+phone_num = '+972585845386'
+# pywhatkit.sendwhatmsg_instantly(phone_no=phone_num,
+#                                 message=Message,
+#                                 wait_time=wait_time_for_writing_msg_in_chrome,
+#                                 tab_close=True,
+#                                 close_time=wait_time_to_close_tab)
 
+save_the_date_img_path = 'save_the_date.jpg'
+pywhatkit.sendwhats_image(receiver='Koala',
+                          img_path='save_the_date_img_path',
+                          caption=Message,
+                          wait_time=wait_time_for_writing_msg_in_chrome,
+                          tab_close=True,
+                          close_time=wait_time_to_close_tab)
 
 
